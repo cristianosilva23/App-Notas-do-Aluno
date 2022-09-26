57a1127119c90063f05ad65c688a1194f7b20c50
Cristiano Silva <84424693+cristianosilva23@users.noreply.github.com>
2022-09-26 13:27:52 -0300

    Add files via upload

---
+++ ExAluno/ExAluno/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExAluno {
+    internal class Aluno {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double Soma() {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public double Falta() {
+            return 60.0 - Soma();
+        }
+
+    }
+}
---
+++ ExAluno/ExAluno/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExAluno {
+    internal class Program {
+        static void Main(string[] args) {
+            Aluno a = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            a.Nome=Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            a.Nota1 = double.Parse(Console.ReadLine());
+            a.Nota2 = double.Parse(Console.ReadLine());
+            a.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nota Final = " + a.Soma().ToString("F2"));
+
+            if (a.Soma() > 60.00) {
+                Console.WriteLine("APROVADO");
+            }
+            else {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + a.Falta().ToString("F2") + " PONTOS");
+
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
